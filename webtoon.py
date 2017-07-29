d27b39f1e5f06bd791d06c9fb76173f2d4a34c80
yeonkevin <yeonkevin@icloud.com>
2017-07-29 19:06:00 +0900

    해당 에피소드 웹툰 이미지 다운로

--- webtoon.py
+++ webtoon.py
@@ -1,4 +1,4 @@
-import requests
+import requests, os
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from itertools import count
@@ -31,7 +31,26 @@ def get_list(title_id):
             }
 
             ep_dict[ep_url] = ep
-            print(ep_name, ep_url)
+            
+            
+def download_img():
+    ep_url = 'http://comic.naver.com/webtoon/detail.nhn?titleId=641253&no=141&weekday=fri'
+    html = requests.get(ep_url).text
+    soup = BeautifulSoup(html, 'html.parser')
 
+    for idx, tag in enumerate(soup.select('.wt_viewer img'), 1):
+        img_url = tag['src']
+
+        headers = {
+            'Referer' : ep_url
+        }
+        img_data = requests.get(img_url, headers=headers).content
+        img_name = str(idx) + '' + os.path.basename(img_url)[-4:]
+
+        with open(img_name, 'wb') as f:
+            f.write(img_data)
+
+        
 
 get_list(641253) #외모 지상 주의
+download_img()
