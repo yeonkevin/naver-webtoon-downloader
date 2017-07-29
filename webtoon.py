623fc6d4836861775d72f5ede8723d2fa2f47d8e
yeonkevin <yeonkevin@icloud.com>
2017-07-29 20:54:04 +0900

    분할된 이미지 합치기 & 소스 정

--- webtoon.py
+++ webtoon.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from itertools import count
 from collections import OrderedDict
+from PIL import Image
 
 def get_list(title_id):
     list_url = 'http://comic.naver.com/webtoon/list.nhn?'
@@ -10,7 +11,7 @@ def get_list(title_id):
 
     for page in count(1):
         params = {
-            'titleId' : 641253,
+            'titleId' : title_id,
             'page' : page,
         }
 
@@ -31,10 +32,10 @@ def get_list(title_id):
             }
 
             ep_dict[ep_url] = ep
-            download_img(ep_url)
-            
-            
-def download_img(urls):
+            ep_download(ep_url)
+
+def ep_download(urls):
+    img_path_list = [] 
     ep_url = urls
     html = requests.get(ep_url).text
     soup = BeautifulSoup(html, 'html.parser')
@@ -43,7 +44,6 @@ def download_img(urls):
         img_url = tag['src']
         webtoon_name =  ' '.join(soup.select('.comicinfo .detail h2')[0].text.split())
         ep_name = soup.select('.tit_area h3')[0].text
-
         headers = {
             'Referer' : ep_url
         }
@@ -51,6 +51,7 @@ def download_img(urls):
         img_name = str(idx) + '' + os.path.basename(img_url)[-4:]
 
         img_path = os.path.join(webtoon_name, ep_name, img_name)
+        img_path_list.append(img_path)
         dir_path = os.path.dirname(img_path)
 
         if not os.path.exists(dir_path):
@@ -63,6 +64,39 @@ def download_img(urls):
             print('DOWNLOAD COMPLETE %s%s' % (ep_name, img_name))
         else:
             print('DOWNLOAD SKIP %s%s' % (ep_name, img_name))
+
+    
+    ep_merge(img_path_list, os.path.join(webtoon_name, ep_name))
         
 
-get_list(641253) #외모 지상 주의
+def ep_merge(ep_list, ep_name):
+    img_path_list = ep_list
+    webtoon_name = ep_name
+    im_list = []
+    merge_path = '{}/merged.png'.format(webtoon_name)
+
+    dir_path = os.path.dirname(merge_path)
+
+    if os.path.isfile(dir_path):
+        print('merged.png는 이미 존재 합니다.')
+        return 0
+
+    for img_path in img_path_list:
+        im = Image.open(img_path)
+        im_list.append(im)
+
+    canvas_size = (max(im.width for im in im_list), sum(im.height for im in im_list))
+
+    canvas = Image.new('RGB', canvas_size)
+
+    left = 0
+    top = 0
+
+    for im in im_list:
+        canvas.paste(im, (left, top))
+        top += im.height
+
+    canvas.save(merge_path)
+
+get_list(696602)
+print('SAVE COMPLETE!!')
