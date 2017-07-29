7d9462a8f327a385b401fcb2413041556969d939
yeonkevin <yeonkevin@icloud.com>
2017-07-29 18:49:54 +0900

    웹툰의 모든 에피소드 이름과 주소 가져오

--- webtoon.py
+++ webtoon.py
@@ -1,20 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from itertools import count
+from collections import OrderedDict
 
-list_url = 'http://comic.naver.com/webtoon/list.nhn?'
+def get_list(title_id):
+    list_url = 'http://comic.naver.com/webtoon/list.nhn?'
+    ep_dict = OrderedDict()
 
-params = {
-    'titleId' : 641253,
-    'pages' : 1
-}
+    for page in count(1):
+        params = {
+            'titleId' : 641253,
+            'page' : page,
+        }
 
-html = requests.get(list_url, params=params).text
-soup = BeautifulSoup(html, 'html.parser')
+        html = requests.get(list_url, params=params).text
+        soup = BeautifulSoup(html, 'html.parser')
 
-tag_list = soup.select('.title > a')
+        tag_list = soup.select('.title > a')
 
-for tag in tag_list:
-    ep_name = tag.text
-    ep_url = urljoin(list_url, tag['href'])
-    print(ep_name, ep_url)
+        for tag in tag_list:
+            ep_name = tag.text
+            ep_url = urljoin(list_url, tag['href'])
+            if ep_url in ep_dict:
+                return ep_dict
+
+            ep = {
+                'url' : ep_url,
+                'name' : ep_name,
+            }
+
+            ep_dict[ep_url] = ep
+            print(ep_name, ep_url)
+
+
+get_list(641253) #외모 지상 주의
