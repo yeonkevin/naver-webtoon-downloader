93196a80db38b8f2191c5d3643a810a39c0541c1
yeonkevin <yeonkevin@icloud.com>
2017-07-29 19:38:49 +0900

    해당웹툰 모든 에피소드 폴더별로 다운로드, 스킵 구

--- webtoon.py
+++ webtoon.py
@@ -31,26 +31,38 @@ def get_list(title_id):
             }
 
             ep_dict[ep_url] = ep
+            download_img(ep_url)
             
             
-def download_img():
-    ep_url = 'http://comic.naver.com/webtoon/detail.nhn?titleId=641253&no=141&weekday=fri'
+def download_img(urls):
+    ep_url = urls
     html = requests.get(ep_url).text
     soup = BeautifulSoup(html, 'html.parser')
 
     for idx, tag in enumerate(soup.select('.wt_viewer img'), 1):
         img_url = tag['src']
+        webtoon_name =  ' '.join(soup.select('.comicinfo .detail h2')[0].text.split())
+        ep_name = soup.select('.tit_area h3')[0].text
 
         headers = {
             'Referer' : ep_url
         }
-        img_data = requests.get(img_url, headers=headers).content
+
         img_name = str(idx) + '' + os.path.basename(img_url)[-4:]
 
-        with open(img_name, 'wb') as f:
-            f.write(img_data)
+        img_path = os.path.join(webtoon_name, ep_name, img_name)
+        dir_path = os.path.dirname(img_path)
 
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        if not os.path.exists(img_path):
+            img_data = requests.get(img_url, headers=headers).content
+            with open(img_path, 'wb') as f:
+                f.write(img_data)
+            print('DOWNLOAD COMPLETE %s%s' % (ep_name, img_name))
+        else:
+            print('DOWNLOAD SKIP %s%s' % (ep_name, img_name))
         
 
 get_list(641253) #외모 지상 주의
-download_img()
